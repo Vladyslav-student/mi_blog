dbc140683bbc99a985c94bbf979894f1d42a575e
Vladyslav Kachko <4830996@alu.murciaeduca.es>
2026-01-29 20:12:29 +0100

    Etapa 5: script personalizado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Mensaje que aparece en la consola cuando se carga la página
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Página cargada correctamente.');
+  console.log('Página cargada correctamente. Mensaje para ver si funciona bien');
 });
